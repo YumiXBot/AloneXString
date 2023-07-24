e02dde6ef1b59b1d45d6fde90d43d130c8aefea0
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-07-25 01:39:39 +0530

    Add files via upload

---
+++ AloneXString/utils/inline.py
@@ -0,0 +1,30 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import SUPPORT_CHAT
+
+
+keyboard = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton(text="𝐆ᴇɴᴇʀᴀᴛᴇ 𝐒ᴇssɪᴏɴ", callback_data="gensession")],
+        [
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="𝐒ᴏᴜʀᴄᴇ", url="https://github.com/TeamAloneOp/AloneXStringGenBot/fork"
+            ),
+        ],
+    ]
+)
+
+gen_key = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v1", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v2", callback_data="pyrogram"),
+        ],
+        [InlineKeyboardButton(text="ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")],
+    ]
+)
+
+retry_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ", callback_data="gensession")]]
+)
