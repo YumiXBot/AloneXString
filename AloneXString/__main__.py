b1fc9b84a6603f88175ef1a72988143744cc225d
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-07-26 11:55:34 +0530

    Add files via upload

--- AloneXString/__main__.py
+++ AloneXString/__main__.py
@@ -3,24 +3,24 @@ import importlib
 
 from pyrogram import idle
 
-from AloneXString import LOGGER, Alone
-from AloneXString.modules import ALL_MODULES
+from Alone import LOGGER, AloneX
+from Alone.modules import ALL_MODULES
 
 
-async def alone_boot():
+async def alonex_boot():
     try:
-        await Alone.start()
+        await AloneX.start()
     except Exception as ex:
         LOGGER.error(ex)
         quit(1)
 
     for all_module in ALL_MODULES:
-        importlib.import_module("AloneXString.modules." + all_module)
+        importlib.import_module("Alone.modules." + all_module)
 
-    LOGGER.info(f"@{Alone.username} Started.")
+    LOGGER.info(f"@{AloneX.username} Started.")
     await idle()
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(alone_boot())
+    asyncio.get_event_loop().run_until_complete(alonex_boot())
     LOGGER.info("Stopping String Gen Bot...")
