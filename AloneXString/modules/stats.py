6d0c5fbb673b077a4993ece8245601e924c83b78
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-07-26 11:43:27 +0530

    Update stats.py

--- AloneXString/modules/stats.py
+++ AloneXString/modules/stats.py
@@ -2,11 +2,11 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from config import OWNER_ID
-from Alone import Alone
+from Alone import AloneX
 from Alone.utils import get_served_users
 
 
-@Alone.on_message(filters.command(["stats", "users"]) & filters.user(OWNER_ID))
+@AloneX.on_message(filters.command(["stats", "users"]) & filters.user(OWNER_ID))
 async def get_stats(_, message: Message):
     users = len(await get_served_users())
-    await message.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {Alone.name} :\n\n {users} ᴜsᴇʀs")
+    await message.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {AloneX.name} :\n\n {users} ᴜsᴇʀs")
