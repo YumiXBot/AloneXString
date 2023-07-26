f52492b77aa5fbbd5a4c42e1ef242877bb80c9b5
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-07-26 11:42:12 +0530

    Update start.py

--- AloneXString/modules/start.py
+++ AloneXString/modules/start.py
@@ -5,10 +5,10 @@ from Alone import AloneX
 from Alone.utils import add_served_user, keyboard
 
 
-@Alone.on_message(filters.command("start") & filters.private & filters.incoming)
+@AloneX.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Alone.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {AloneX.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
