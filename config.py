baba4dc46f42868e2d751652fcfa5b754e1db303
YumiXBot <146187449+YumiXBot@users.noreply.github.com>
2023-10-01 16:17:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "17596251"))
+API_HASH = getenv("API_HASH", "e58343b4c0193e293e391daf97603fcd")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6467944590:AAGd6n4ELIPe-y8BOk9e7tB-pBhw4oh3BZA")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hny:zara@cluster0.lfe5o.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 6079943111))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneXBots")
